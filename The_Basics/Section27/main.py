0969c17e78e32b9f1fe355af8891ead3fa35112a
LesterBryanIlao <lestertetengilao@gmail.com>
2023-01-07 15:04:35 +0800

    Input management

--- The_Basics/Section27/main.py
+++ The_Basics/Section27/main.py
@@ -2,18 +2,42 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.gridlayout import GridLayout
+import json
+from datetime import datetime
+
 Builder.load_file('design.kv')
 
+
 class LoginScreen(Screen):
-    pass
+
+    def sign_up(self):
+        self.manager.current = "signup_screen"
+
+
+class SignUpScreen(Screen):
+    def submit(self):
+        self.manager.current = "login_screen"
+
+    def add_user(self, username, password):
+        username = username.strip()
+        password = password.strip()
+        with open("users.json") as file:
+            users = json.load(file)
+        print(users)
+
+        users[username] = {"username": username,
+                           "password": password, 
+                           "created": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+        print(users)
 
 class RootWidget(ScreenManager):
     pass
 
+
 class MainApp(App):
-    
     def build(self):
         return RootWidget()
+
 
 if __name__ == '__main__':
     MainApp().run()
