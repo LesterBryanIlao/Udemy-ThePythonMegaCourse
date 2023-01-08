0218172f7cd23df0ad6c253579bad484402e3f94
LesterBryanIlao <lestertetengilao@gmail.com>
2023-01-08 19:47:05 +0800

    Added info in message

--- The_Basics/Section30/app.py
+++ The_Basics/Section30/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from send_email import send_email
-
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:superuser@localhost/height_collector'
@@ -28,13 +28,18 @@ def index():
 def success():
     if request.method == 'POST':
         email = request.form["email_name"]
-        height = request.form["height_name"]
-        send_email(email, height)
+        height = round(float(request.form["height_name"]),1)
+        
         if db.session.query(Data).filter(Data.email_ == email).count() == 0:
             data = Data(email, height)
             db.session.add(data)
             db.session.commit()
 
+            avg_height = db.session.query(func.avg(Data.height_)).scalar()
+            avg_height = round(avg_height, 1)
+
+            count = db.session.query(Data.height_).count()
+            send_email(email, height, avg_height, count)
             return render_template('success.html')
 
     return render_template('index.html', text="Email already provided a response.")
